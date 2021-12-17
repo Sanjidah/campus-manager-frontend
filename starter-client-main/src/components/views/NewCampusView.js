20d077fa4d37ebc65e2651a63027a41aafd6d1df
sanjidah <sanjidah42@gmail.com>
2021-12-17 17:07:53 -0500

    Created navigation bar for Campus Page

--- starter-client-main/src/components/views/NewCampusView.js
+++ starter-client-main/src/components/views/NewCampusView.js
@@ -1,22 +1,50 @@
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles( () => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Georgia, serif', 
+    fontSize: '35px', 
+    color: '#9ebe35'
+  },
+  appBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#f6f6f6',
+    fontFamily: 'Georgia, serif', 
+    width: "50%",
+    margin: "auto",
+  },
+  links:{
+    textDecoration: 'none',
+  },
   formContainer:{  
     width: '500px',
     backgroundColor: '#f0f0f5',
     borderRadius: '5px',
     margin: 'auto',
   },
-  title: {
-    flexGrow: 1,
-    textAlign: 'left',
-    textDecoration: 'none'
-  }, 
+  // title: {
+  //   flexGrow: 1,
+  //   textAlign: 'left',
+  //   textDecoration: 'none'
+  // }, 
   customizeAppBar:{
     backgroundColor: '#11153e',
     shadows: ['none'],
@@ -37,6 +65,32 @@ const NewCampusView = (props) => {
 
   return (
     <div className={classes.root}>
+
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="#9ebe35" >
+            Campus Manager
+            <Link className={classes.links} to={'/'} >
+              <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+                Home
+              </Button>
+            </Link>
+          </Typography>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              All Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      
       <div className={classes.formContainer}>
         <div className={classes.formTitle}>
           <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
