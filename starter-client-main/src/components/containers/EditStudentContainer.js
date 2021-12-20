a368a1dfbc211ca313bf55297ef91cc18e1d8b29
mariamkhan-dev <mariamkhanofficial@gmail.com>
2021-12-19 22:29:53 -0500

    edit student functional

--- starter-client-main/src/components/containers/EditStudentContainer.js
+++ starter-client-main/src/components/containers/EditStudentContainer.js
@@ -5,17 +5,18 @@ import { Redirect } from 'react-router-dom';
 import EditStudentView from '../views/EditStudentView';
 import { editStudentThunk } from '../../store/thunks';
 import { fetchStudentThunk } from "../../store/thunks";
+import { editStudent } from '../../store/actions/actionCreators';
 
 
 class EditStudentContainer extends Component {
     constructor(props){
         super(props);
         this.state = {
-          firstname: "", 
-          lastname: "", 
-          email: "",
-          gpa: 0,
-          campusId: null, 
+          firstname: this.props.student.firstname, 
+          lastname: this.props.student.lastname, 
+          email: this.props.student.email,
+          gpa: this.props.student.gpa,
+          campusId: this.props.student.campusId, 
           redirect: false, 
           redirectId: null
         };
@@ -40,21 +41,29 @@ class EditStudentContainer extends Component {
             lastname: this.state.lastname,
             campusId: this.state.campusId,
             email: this.state.email,
-            gpa: this.state.gpa
+            gpa: this.state.gpa,
+            id: this.props.student.id
         };
-        console.log(this.props)
+
+        console.log("student: ")
+        console.log(student)
+
+        console.log("this.props.student: ")
+        console.log(this.props.student)
 
         let newStudent = await this.props.editStudent(student);
+
+        console.log("newStudent: ")
         console.log(newStudent)
 
         this.setState({
           firstname: "", 
           lastname: "", 
-          campusId: null, 
           email: "",
           gpa: 0,
+          campusId: null, 
           redirect: true, 
-          redirectId: this.props.match.params.id
+            redirectId: newStudent.id
         });
     }
 
@@ -87,7 +96,6 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
     return({
-        addStudent: (student) => dispatch(editStudentThunk(student)),
         fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
         editStudent: (student) => dispatch(editStudentThunk(student))
     })
