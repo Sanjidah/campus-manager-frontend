a737afc6c9f5162e9ea9ea7f502518d6bc644b94
sanjidah <sanjidah42@gmail.com>
2021-12-20 02:49:17 -0500

    Revert "Merge pull request #10 from Sanjidah/all-campus-css-update"
    This reverts commit 240f6d9f8dbd9ca0a5b1afde14ae0fbc8f2b651f, reversing
    changes made to d78bdb693d5d1a240b4c324b6391a01c57b32c8a.

--- starter-client-main/src/components/views/AllCampusesView.js
+++ starter-client-main/src/components/views/AllCampusesView.js
@@ -1,191 +1,128 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  campus: {
-    textAlign: "center",
-    color: "white",
-    backgroundColor: "#45A29E",
-    height: "auto",
-    width: "400px",
-    borderRadius: "40px",
-  },
-  createButton: {
-    backgroundColor: "pink",
-  },
-  campusButton: {
-    marginLeft: "20px",
-    marginRight: "20px",
-    marginTop: "20px",
-    backgroundColor: "#45A29E",
-    borderRadius: "40px",
-  },
   title: {
     flexGrow: 1,
-    textAlign: "left",
-    fontType: "bold",
-    fontFamily: "Georgia, serif",
-    fontSize: "80px",
-    color: "#66FCF1",
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Georgia, serif', 
+    fontSize: '35px', 
+    color: '#9ebe35'
   },
-  appBar: {
-    backgroundColor: "#1F2833",
-    shadows: ["none"],
+  appBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
   },
-  greeting: {
-    marginTop: "20px",
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "#f6f6f6",
-    fontFamily: "Georgia, serif",
-    width: "0%",
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#f6f6f6',
+    fontFamily: 'Georgia, serif', 
+    width: "50%",
     margin: "auto",
   },
-  links: {
-    textDecoration: "none",
+  links:{
+    textDecoration: 'none',
   },
-  image: {
-    width: "200px",
+  image:{  
+    width: '200px',
     height: "auto",
-  },
+  }
 }));
 
 const AllCampusesView = (props) => {
-  const { campuses, deleteCampus } = props;
+  const {campuses, deleteCampus} = props;
   const classes = useStyles();
   if (!props.allCampuses.length) {
-    return (
-      <div>
-        <AppBar position="static" elevation={0} className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" className={classes.title} color="#9ebe35">
+    return <div>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+        <Typography variant="h6" className={classes.title} color="#9ebe35" >
               Campus Manager
-            </Typography>
-
-            <Link className={classes.links} to={"/"}>
-              <Button
-                variant="contained"
-                color="white"
-                style={{ marginRight: "10px", height: "60px", width: "200px" }}
-              >
-                Home
-              </Button>
+        </Typography>
+  
+            <Link className={classes.links} to={'/'} >
+                <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+                  Home
+                </Button>
             </Link>
 
-            <Link className={classes.links} to={"/campuses"}>
-              <Button
-                variant="contained"
-                color="white"
-                style={{ marginRight: "10px", height: "60px", width: "200px" }}
-              >
-                All Campuses
-              </Button>
-            </Link>
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
 
-            <Link className={classes.links} to={"/students"}>
-              <Button
-                variant="contained"
-                color="white"
-                style={{ marginRight: "10px", height: "60px", width: "200px" }}
-              >
-                All Students
-              </Button>
-            </Link>
-          </Toolbar>
-        </AppBar>
-
-        <p>There are no campuses.</p>
-        <Link to={`/newcampus`}>
-          <Button
-            variant="contained"
-            color="white"
-            style={{ marginRight: "10px", height: "60px", width: "200px" }}
-          >
-            <button>Add New Campus</button>
-          </Button>
-        </Link>
-      </div>
-    );
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              All Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      
+      <p>There are no campuses.</p>
+      <Link to={`/newcampus`}>
+        <button>Add New Campus</button>
+      </Link>
+      </div>;
   }
 
   return (
     <div>
       <AppBar position="static" elevation={0} className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="#9ebe35">
+          <Typography variant="h6" className={classes.title} color="#9ebe35" >
             Campus Manager
           </Typography>
 
-          <Link className={classes.links} to={"/"}>
-            <Button
-              variant="contained"
-              color="white"
-              style={{ marginRight: "10px", height: "60px", width: "200px" }}
-            >
-              Home
-            </Button>
+          <Link className={classes.links} to={'/'} >
+              <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+                Home
+              </Button>
           </Link>
 
-          <Link className={classes.links} to={"/campuses"}>
-            <Button
-              variant="contained"
-              ccolor="white"
-              style={{ marginRight: "10px", height: "60px", width: "200px" }}
-            >
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
               Campuses
             </Button>
           </Link>
 
-          <Link className={classes.links} to={"/students"}>
-            <Button
-              variant="contained"
-              color="white"
-              style={{ marginRight: "10px", height: "60px", width: "200px" }}
-            >
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
               Students
             </Button>
           </Link>
         </Toolbar>
       </AppBar>
-      <div className={classes.greeting}>
-        <Link className={classes.links} to={"/newcampus"}>
-          <Button
-            variant="contained"
-            color="white"
-            style={{ marginRight: "10px", height: "60px", width: "200px" }}
-          >
-            Add new Campus!
-          </Button>
-        </Link>
-      </div>
 
       {props.allCampuses.map((campus) => (
-        <button className={classes.campusButton}>
-          <div className={classes.campus}>
-            <div key={campus.id}>
-              <Link to={`/campus/${campus.id}`}>
-                <h1>{campus.name}</h1>
-              </Link>
-              <img
-                src={campus.imageUrl}
-                alt="Campus"
-                className={classes.image}
-              />
-              <p>{campus.description}</p>
-              <button onClick={() => deleteCampus(campus.id)}>Delete</button>
-            </div>
-          </div>
-        </button>
+        <div key={campus.id}>
+          <Link to={`/campus/${campus.id}`}>
+            <h1>{campus.name}</h1>
+          </Link>
+          <img src={campus.imageUrl} alt="Campus" className={classes.image}/>
+          <p>{campus.description}</p>
+          <button onClick={() => deleteCampus(campus.id)}>Delete</button>
+        </div>
       ))}
+      <p>
+        <Link to={`/newcampus`}>
+          <button>Add New Campus</button>
+        </Link>
+      </p>
+
     </div>
   );
 };
