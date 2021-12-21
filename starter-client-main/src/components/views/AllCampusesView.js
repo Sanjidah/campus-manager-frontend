efa3a64beac202ecadd091e585004fa5b6a8134d
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2021-12-20 20:17:14 -0500

    test

--- starter-client-main/src/components/views/AllCampusesView.js
+++ starter-client-main/src/components/views/AllCampusesView.js
@@ -1,105 +1,122 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/core/styles";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   title: {
     flexGrow: 1,
-    textAlign: 'left',
-    fontType: 'bold',
-    fontFamily: 'Georgia, serif', 
-    fontSize: '35px', 
-    color: '#9ebe35'
+    textAlign: "left",
+    fontType: "bold",
+    fontFamily: "Georgia, serif",
+    fontSize: "35px",
+    color: "#9ebe35",
   },
-  appBar:{
-    backgroundColor: '#11153e',
-    shadows: ['none'],
+  appBar: {
+    backgroundColor: "#11153e",
+    shadows: ["none"],
   },
-  greeting:{
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: '#f6f6f6',
-    fontFamily: 'Georgia, serif', 
+  greeting: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#f6f6f6",
+    fontFamily: "Georgia, serif",
     width: "50%",
     margin: "auto",
   },
-  links:{
-    textDecoration: 'none',
+  links: {
+    textDecoration: "none",
   },
-  image:{  
-    width: '200px',
+  image: {
+    width: "200px",
     height: "auto",
-  }
+  },
 }));
 
 const AllCampusesView = (props) => {
-  const {campuses, deleteCampus} = props;
+  const { campuses, deleteCampus } = props;
   const classes = useStyles();
   if (!props.allCampuses.length) {
-    return <div>
-      <AppBar position="static" elevation={0} className={classes.appBar}>
-        <Toolbar>
-        <Typography variant="h6" className={classes.title} color="#9ebe35" >
+    return (
+      <div>
+        <AppBar position="static" elevation={0} className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title} color="#9ebe35">
               Campus Manager
-        </Typography>
-  
-            <Link className={classes.links} to={'/'} >
-                <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
-                  Home
-                </Button>
+            </Typography>
+
+            <Link className={classes.links} to={"/"}>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginRight: "10px" }}
+              >
+                Home
+              </Button>
             </Link>
 
-          <Link className={classes.links} to={'/campuses'} >
-            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
-              All Campuses
-            </Button>
-          </Link>
+            <Link className={classes.links} to={"/campuses"}>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginRight: "10px" }}
+              >
+                All Campuses
+              </Button>
+            </Link>
 
-          <Link className={classes.links} to={'/students'} >
-            <Button variant="contained" color="primary">
-              All Students
-            </Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      
-      <p>There are no campuses.</p>
-      <Link to={`/newcampus`}>
-        <button>Add New Campus</button>
-      </Link>
-      </div>;
+            <Link className={classes.links} to={"/students"}>
+              <Button variant="contained" color="primary">
+                All Students
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+
+        <p>There are no campuses.</p>
+        <Link to={`/newcampus`}>
+          <button>Add New Campus</button>
+        </Link>
+      </div>
+    );
   }
 
   return (
     <div>
       <AppBar position="static" elevation={0} className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="#9ebe35" >
+          <Typography variant="h6" className={classes.title} color="#9ebe35">
             Campus Manager
           </Typography>
 
-          <Link className={classes.links} to={'/'} >
-              <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
-                Home
-              </Button>
+          <Link className={classes.links} to={"/"}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginRight: "10px" }}
+            >
+              Home
+            </Button>
           </Link>
 
-          <Link className={classes.links} to={'/campuses'} >
-            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+          <Link className={classes.links} to={"/campuses"}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginRight: "10px" }}
+            >
               Campuses
             </Button>
           </Link>
 
-          <Link className={classes.links} to={'/students'} >
+          <Link className={classes.links} to={"/students"}>
             <Button variant="contained" color="primary">
               Students
             </Button>
@@ -112,7 +129,7 @@ const AllCampusesView = (props) => {
           <Link to={`/campus/${campus.id}`}>
             <h1>{campus.name}</h1>
           </Link>
-          <img src={campus.imageUrl} alt="Campus" className={classes.image}/>
+          <img src={campus.imageUrl} alt="Campus" className={classes.image} />
           <p>{campus.description}</p>
           <button onClick={() => deleteCampus(campus.id)}>Delete</button>
         </div>
@@ -122,7 +139,6 @@ const AllCampusesView = (props) => {
           <button>Add New Campus</button>
         </Link>
       </p>
-
     </div>
   );
 };
